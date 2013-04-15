48dc4c12d375c6cd84d3103a039298c1dbee9c72
Pascal Spörri <pascal.spoerri@gmail.com>
2013-04-15 22:43:54 +0200

    Added function to set cpu frequency

---
+++ hw05/code/set_cpu_frequency.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Available frequencies
+# cat /sys/devices/system/cpu/*/cpufreq/scaling_available_frequencies
+# Inspired by: http://www.pantz.org/software/cpufreq/usingcpufreqonlinux.html
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq"
+
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq"
+sh -c "echo 2600000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq"
+
