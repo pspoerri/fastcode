2c620e047aea29ac0fdbadcf8ac9a1cd3bc9d10e
Pascal Spörri <pascal.spoerri@gmail.com>
2013-04-16 11:00:58 +0200

    Added plot

---
+++ hw05/code/plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+#filename = 'run_13_04_16__10_10.pickle'
+#filename = 'run_13_04_16__10_23.pickle'
+filename = 'run_13_04_16__10_48.pickle'
+
+fig = plt.figure()
+
+f = open(filename, 'r')
+runtime_data = pickle.load(f) 
+plot_refs = []
+legend_refs = []
+for f,results in runtime_data.iteritems():
+    
+    d = map(lambda l: (int(l['block_size']), float(l['flops_cycle'])), results)
+    d.sort()
+    block_sizes, flops_cycle = zip(*d)
+#print block_sizes
+#    print flops_cycle
+
+    p = plt.plot(block_sizes, flops_cycle, label=str(f))
+plt.legend(loc='best')
+plt.xlabel('Block Size') 
+plt.title('Flops/Cycle')
+plt.show()
+
+
