f834ff9c0ac2bd49582f5cac9f1f215bd7b27b7d
Pascal Spörri <pascal.spoerri@gmail.com>
2013-04-16 13:48:38 +0200

    Added plot tool for final code

---
+++ hw05/code/plot_final.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+#filename = 'run_13_04_16__10_10.pickle'
+#filename = 'run_13_04_16__10_23.pickle'
+filename = 'matrix_run_13_04_16__13_45.pickle'
+
+fig = plt.figure()
+
+f = open(filename, 'r')
+runtime_data = pickle.load(f) 
+plot_refs = []
+legend_refs = []
+for f,results in runtime_data.iteritems():
+    
+    d = map(lambda l: (int(l['matrix_size']), float(l['flops_cycle'])), results)
+    d.sort()
+    print f
+    print d
+    matrix_sizes, flops_cycle = zip(*d)
+#print block_sizes
+#    print flops_cycle
+    block_size = results[0]['block_size']
+
+    p = plt.plot(matrix_sizes, flops_cycle, label="Block Size: "+str(block_size))
+plt.legend(loc='best')
+plt.xlabel('Matrix Size') 
+plt.title('Flops/Cycle')
+plt.show()
+
+
