7633dc839ab2d02e2b1358a83845d5c5c16769e7
Pascal Spörri <pascal.spoerri@gmail.com>
2013-05-09 22:29:20 +0200

    updated auto.asm

--- hw06/warmup/auto.asm
+++ hw06/warmup/auto.asm
@@ -6,16 +6,23 @@
 warmup:
 .LFB0:
 	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	andq	$-32, %rsp
+	addq	$16, %rsp
 	testl	%edx, %edx
 	jle	.L1
 	movl	%edx, %r9d
 	movl	%edx, %ecx
-	shrl	$2, %r9d
-	leal	0(,%r9,4), %r8d
+	shrl	$3, %r9d
+	leal	0(,%r9,8), %r8d
 	testl	%r8d, %r8d
 	je	.L6
 	leaq	(%rdi,%rcx,8), %rax
-	cmpl	$5, %edx
+	cmpl	$7, %edx
 	leaq	(%rsi,%rcx,4), %rcx
 	seta	%r10b
 	cmpq	%rax, %rsi
@@ -25,30 +32,39 @@ warmup:
 	orl	%ecx, %eax
 	testb	%al, %r10b
 	je	.L6
-	movaps	%xmm0, %xmm1
-	movaps	.LC0(%rip), %xmm4
+	vmovaps	.LC0(%rip), %ymm6
 	xorl	%eax, %eax
-	shufps	$0, %xmm1, %xmm1
 	xorl	%ecx, %ecx
-	movaps	%xmm1, %xmm5
+	vshufps	$0, %xmm0, %xmm0, %xmm7
+	vinsertf128	$1, %xmm7, %ymm7, %ymm7
 	.p2align 4,,10
 	.p2align 3
 .L4:
-	movups	(%rdi,%rax,2), %xmm2
+	vmovups	(%rdi,%rax,2), %xmm4
 	addl	$1, %ecx
-	movups	16(%rdi,%rax,2), %xmm3
-	movaps	%xmm2, %xmm1
-	shufps	$221, %xmm3, %xmm2
-	divps	%xmm5, %xmm2
-	shufps	$136, %xmm3, %xmm1
-	mulps	%xmm4, %xmm1
-	addps	%xmm2, %xmm1
-	movups	%xmm1, (%rsi,%rax)
-	addq	$16, %rax
+	vmovups	32(%rdi,%rax,2), %xmm2
+	vinsertf128	$0x1, 16(%rdi,%rax,2), %ymm4, %ymm4
+	vinsertf128	$0x1, 48(%rdi,%rax,2), %ymm2, %ymm2
+	vshufps	$136, %ymm2, %ymm4, %ymm3
+	vshufps	$221, %ymm2, %ymm4, %ymm2
+	vperm2f128	$3, %ymm3, %ymm3, %ymm1
+	vshufps	$68, %ymm1, %ymm3, %ymm5
+	vshufps	$238, %ymm1, %ymm3, %ymm1
+	vperm2f128	$32, %ymm1, %ymm5, %ymm1
+	vmulps	%ymm6, %ymm1, %ymm3
+	vperm2f128	$3, %ymm2, %ymm2, %ymm1
+	vshufps	$68, %ymm1, %ymm2, %ymm4
+	vshufps	$238, %ymm1, %ymm2, %ymm1
+	vperm2f128	$32, %ymm1, %ymm4, %ymm1
+	vdivps	%ymm7, %ymm1, %ymm1
+	vaddps	%ymm1, %ymm3, %ymm1
+	vmovups	%xmm1, (%rsi,%rax)
+	vextractf128	$0x1, %ymm1, 16(%rsi,%rax)
+	addq	$32, %rax
 	cmpl	%ecx, %r9d
 	ja	.L4
 	cmpl	%r8d, %edx
-	je	.L10
+	je	.L1
 .L3:
 	leal	(%r8,%r8), %ecx
 	movslq	%r8d, %r9
@@ -59,31 +75,36 @@ warmup:
 	.p2align 4,,10
 	.p2align 3
 .L5:
-	movss	4(%rcx,%rax,8), %xmm2
-	movss	(%rcx,%rax,8), %xmm1
-	divss	%xmm0, %xmm2
-	addss	%xmm1, %xmm1
-	addss	%xmm2, %xmm1
-	movss	%xmm1, (%rdi,%rax,4)
+	vmovss	4(%rcx,%rax,8), %xmm2
+	vmovss	(%rcx,%rax,8), %xmm1
+	vdivss	%xmm0, %xmm2, %xmm2
+	vaddss	%xmm1, %xmm1, %xmm1
+	vaddss	%xmm2, %xmm1, %xmm1
+	vmovss	%xmm1, (%rdi,%rax,4)
 	addq	$1, %rax
 	leal	(%r8,%rax), %esi
 	cmpl	%esi, %edx
 	jg	.L5
 .L1:
-	rep
+	leave
+	.cfi_remember_state
+	.cfi_def_cfa 7, 8
+	vzeroupper
 	ret
 .L6:
+	.cfi_restore_state
 	xorl	%r8d, %r8d
 	jmp	.L3
-.L10:
-	.p2align 4,,8
-	ret
 	.cfi_endproc
 .LFE0:
 	.size	warmup, .-warmup
-	.section	.rodata.cst16,"aM",@progbits,16
-	.align 16
+	.section	.rodata.cst32,"aM",@progbits,32
+	.align 32
 .LC0:
+	.long	1073741824
+	.long	1073741824
+	.long	1073741824
+	.long	1073741824
 	.long	1073741824
 	.long	1073741824
 	.long	1073741824
