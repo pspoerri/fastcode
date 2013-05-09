28110705700ababc48cb4cb4004a31c15538440f
Pascal Spörri <pascal@spoerri.io>
2013-05-09 22:23:29 +0200

    Added alignment information

--- hw06/warmup/base.c
+++ hw06/warmup/base.c
@@ -1,6 +1,9 @@
 void warmup(float *x, float *y, int size, float alpha)
 {
+    #pragma ivdep
     int i;
+
+    #pragma vector aligned
     for (i=0; i<size; i++)
     {
         y[i] = x[2*i]+x[2*i]+x[2*i+1]/alpha;
