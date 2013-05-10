74675e32602332e7544723d2a23bc0616ae3aeb5
Pascal Spörri <pascal.spoerri@gmail.com>
2013-05-10 09:38:22 +0200

    Added additional manual code

--- hw06/vectorization/manual1.c
+++ hw06/vectorization/manual1.c
@@ -7,11 +7,18 @@ void FIR(float *y, float *x, float h0, float h1, float h2, float h3, int size)
     __m256 m1 = _mm256_set_ps(0.0,0.0,0.0, h0, h1, h2, h3,0.0);
     __m256 m2 = _mm256_set_ps(0.0,0.0, h0, h1, h2, h3,0.0,0.0);
     __m256 m3 = _mm256_set_ps(0.0, h0, h1, h2, h3,0.0,0.0,0.0);
-    
+
     // use 32byte alignment
+    __m128 k1 = _mm_load_ps(x);
+    __m128 k2;
     for (i=0; i<size-3; i+=4)
     {
-        __m256 l = _mm256_loadu_ps(x+i);
+        //__m256 l = _mm256_loadu_ps(x+i);
+        k2 = _mm_loadu_ps(x+i+4);
+        __m256 l; // = _mm256_loadu_ps(x+i);
+        l = _mm256_insertf128_ps(l, k1, 0);
+        l = _mm256_insertf128_ps(l, k2, 1);
+        k1 = k2;
         __m256 a0 = _mm256_mul_ps(l,m0); 
         __m256 a1 = _mm256_mul_ps(l,m1); 
         __m256 a2 = _mm256_mul_ps(l,m2); 
@@ -21,13 +28,12 @@ void FIR(float *y, float *x, float h0, float h1, float h2, float h3, int size)
         __m256 z1 = _mm256_hadd_ps(a1, _mm256_permute2f128_ps( a1 , a1 , 1));
         __m256 z2 = _mm256_hadd_ps(a2, _mm256_permute2f128_ps( a2 , a2 , 1));
         __m256 z3 = _mm256_hadd_ps(a3, _mm256_permute2f128_ps( a3 , a3 , 1));
-        
 
         __m256 p0 = _mm256_hadd_ps(a0, z1);
         __m256 p1 = _mm256_hadd_ps(z2, z3);
 
         __m256 res = _mm256_hadd_ps(p0, p1);
-        
+
         __m128 s = _mm256_extractf128_ps (res, 0);
         _mm_store_ps(y+i,s); // store it
     }
